c0d8fa5b0582154839eb067f0bd508587bae38c1
Adriel Adonis <arodriguez32@fisher.edu>
2020-10-04 19:21:52 -0400

    first heroku commit

--- octopi_app_api/config/routes.rb
+++ octopi_app_api/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :users do
     collection do
       post '/login', to: 'users#login'
-  resources :portfolios
+      post '/create', to: 'users#create'
     end
   end
 end
